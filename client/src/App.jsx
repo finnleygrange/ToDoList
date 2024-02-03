1bd78803663985124c2c8d3b19f6e62f911ca890
Finnley Grange <dev.finnleygrange@icloud.com>
2024-02-03 01:29:40 +0000

    Store list in browser using localstorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import DateNow from "./components/DateNow";
 import AddItemForm from "./components/AddItemForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ItemList from "./components/ItemList";
 
 function App() {
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const storedItems = JSON.parse(localStorage.getItem("items"));
+    if (storedItems) {
+      setItems(storedItems);
+    }
+  }, []);
+
   function AddItemToList(newItem) {
-    setItems([...items, newItem]);
+    const updatedItems = [...items, newItem];
+    setItems(updatedItems);
+    localStorage.setItem("items", JSON.stringify(updatedItems));
   }
 
   return (
